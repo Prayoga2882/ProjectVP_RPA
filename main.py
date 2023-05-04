74d56277033a90e6a0568b1c04ad342ed9db2d94
Prayoga Volantis <112841946+PrayogaBoedihartoyo28@users.noreply.github.com>
2023-05-04 11:07:58 +0700

    Mini_Project_II

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from Get_Rate.index import *
 
 root = tk.Tk()
 root.title("VIA PULSA GLOBAL INDONESIA")
-root.geometry("700x400")
+root.geometry("900x400")
 input_label = tk.Label(root, text="MINI PROJECT II")
 input_label.grid(row=0, columnspan=3)
 
@@ -26,7 +26,7 @@ button = tk.Button(root, text="Generate Rate Sukma Convert!", command=get_rate_s
 button.grid(row=2, column=2, padx=5, pady=5)
 
 button = tk.Button(root, text="Generate Rate Zahra Convert!", command=get_rate_zahra_convert, activebackground='yellow')
-button.grid(row=2, column=3, padx=5, pady=5)
+button.grid(row=3, column=2, padx=5, pady=5)
 
 button = tk.Button(root, text="Generate Rate CV Convert!", command=get_rate_cv_convert, activebackground='yellow')
 button.grid(row=3, column=0, padx=5, pady=5)
@@ -35,7 +35,7 @@ button = tk.Button(root, text="Generate Rate Tentra Pulsa!", command=get_rate_te
 button.grid(row=3, column=1, padx=5, pady=5)
 
 button = tk.Button(root, text="Generate Rate Conversa!", command=get_rate_conversa, activebackground='yellow')
-button.grid(row=3, column=2, padx=5, pady=5)
+button.grid(row=2, column=4, padx=5, pady=5)
 
 result_label = tk.Label(root, text="")
 result_label.grid(row=4, columnspan=4)
